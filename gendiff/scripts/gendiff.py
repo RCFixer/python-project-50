a414a126750b694e31a0c56852452800150754f4
roman <roman.bogdanov@lanit-tercom.com>
2024-02-12 15:29:51 +0300

    updated helper

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,6 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 parser.add_argument('file', metavar='first_file')
 parser.add_argument('file', metavar='second_file')
+parser.add_argument('-f', '--format', help='set format of output')
 
 args = parser.parse_args()
 
