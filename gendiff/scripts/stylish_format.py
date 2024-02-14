36cd3720103632f83e993f39ec3f85a426be0e35
roman <roman.bogdanov@lanit-tercom.com>
2024-02-14 09:23:55 +0300

    hotfix

--- gendiff/scripts/stylish_format.py
+++ gendiff/scripts/stylish_format.py
@@ -10,6 +10,7 @@ def convert_values(value):
 def stylish(diff, indentation=0):
     result = []
     spaces = (indentation + 1) * 4 * ' '
+    final_spaces = spaces
     for key, value in diff.items():
         match key[0]:
             case '=':
@@ -17,21 +18,25 @@ def stylish(diff, indentation=0):
                     result.append(f'{spaces[2:]}  {key[1]}: {stylish(value, indentation + 1)}')
                 else:
                     result.append(f'{spaces[2:]}  {key[1]}: {convert_values(value)}')
+                final_spaces = spaces[4:]
             case '-':
                 if isinstance(value, dict):
                     result.append(f'{spaces[2:]}- {key[1]}: {stylish(value, indentation + 1)}')
                 else:
                     result.append(f'{spaces[2:]}- {key[1]}: {convert_values(value)}')
+                final_spaces = spaces[6:]
             case '+':
                 if isinstance(value, dict):
                     result.append(f'{spaces[2:]}+ {key[1]}: {stylish(value, indentation + 1)}')
                 else:
                     result.append(f'{spaces[2:]}+ {key[1]}: {convert_values(value)}')
+                final_spaces = spaces[4:]
             case _:
                 if isinstance(value, dict):
                     result.append(f'{spaces}{key}: {stylish(value, indentation + 1)}')
                 else:
                     result.append(f'{spaces}{key}: {convert_values(value)}')
+                final_spaces = spaces[4:]
     if indentation == 0:
-        spaces = ''
-    return '{\n' + '\n'.join(result) + '\n' + spaces + '}'
+        final_spaces = ''
+    return '{\n' + '\n'.join(result) + '\n' + final_spaces + '}'
